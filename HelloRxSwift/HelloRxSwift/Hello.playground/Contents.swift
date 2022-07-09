b0b3678e1f12700298085d07b2047f34a6a06853
Bran <cold929@naver.com>
2022-07-09 16:41:19 +0900

    chore: -

--- HelloRxSwift/HelloRxSwift/Hello.playground/Contents.swift
+++ HelloRxSwift/HelloRxSwift/Hello.playground/Contents.swift
@@ -44,11 +44,15 @@ subject.onNext("Subject")
 //subject.onCompleted()
 subject.onError(MyError.error)
 
+// Observable의 life Cycle이 종료되었기 때문에 바로종료
 let o3 = subject.subscribe { print(">>3", $0) }
+subject.onNext("test")
 o3.disposed(by: disposeBag)
 
 
 // MARK: -
+
+// 하나의 값을 방출하는 경우
 Observable.just("🤦")
   .subscribe { event in print(event) }
   .disposed(by: disposeBag)
@@ -57,11 +61,58 @@ Observable.just([1, 2, 3]) // 그대로 방출함
   .subscribe { event in print(event) }
   .disposed(by: disposeBag)
 
-Observable.of(1, 2, 3)
+// 하나이상의 값을 방출하는 경우
+Observable.of(1, 2, 3) // 가변 파라미터로 선언되어 있음
   .subscribe { event in print(event) }
   .disposed(by: disposeBag)
 
+// 배열을 방출하는 경우?
 Observable.from([1, 2, 3])
   .subscribe { event in print(event) }
   .disposed(by: disposeBag)
+
+// MARK: -
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+Observable.from(numbers)
+  .filter { $0 % 2 == 0 }
+  .subscribe { print( $0) }
+  .disposed(by: disposeBag)
+
+
+let a = BehaviorSubject(value: 1)
+let b = BehaviorSubject(value: 2)
+
+let subjects = PublishSubject<BehaviorSubject<Int>>()
+subjects
+  .flatMap { $0.asObservable() }
+  .subscribe { print($0) }
+  .disposed(by: disposeBag)
+
+subjects.onNext(a)
+subjects.onNext(b)
+
+// MARK: -
+
+let greetings = PublishSubject<String>()
+let lanuages = PublishSubject<String>()
+
+Observable.combineLatest(greetings, lanuages) { lhs, rhs
+  -> String in
+  return "\(lhs) \(rhs)"
+}
+.subscribe{ print($0) }
+.disposed(by: disposeBag)
+
+greetings.onNext("Hi")
+lanuages.onNext("World")
+greetings.onNext("Hello")
+greetings.onCompleted()
+//greetings.onError(MyError.error)
+lanuages.onNext("SwiftUI")
+lanuages.onCompleted()
+// completed는 2개 다 complete가 와야 되지만
+// error는 둘 중 하나만 오면 된다
+
 
--- HelloRxSwift/HelloRxSwift/ViewController.swift
+++ HelloRxSwift/HelloRxSwift/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
+
 class ViewController: UIViewController {
+
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var label: UILabel!
+  let disposeBag = DisposeBag()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
+    label.text = ""
+    textField.becomeFirstResponder()
 
+//     Main-Thread에서 실행되는가?
+//    textField.rx.text
+//      .observeOn(MainScheduler.instance)
+//      .subscribe(onNext: { [weak self] str in
+//        self?.label.text = str
+//      })
+//      .disposed(by: disposeBag)
+
+    textField.rx.text
+      .bind(to: label.rx.text) // 메인쓰레드를 직접 지정하지 않아도 된다
+      .disposed(by: disposeBag)
+  }
 
 }
 
