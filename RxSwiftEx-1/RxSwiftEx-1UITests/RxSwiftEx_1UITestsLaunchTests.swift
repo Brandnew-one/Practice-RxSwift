9f8c042a3c3216b33999161c834156be3227abc9
Bran <cold929@naver.com>
2022-07-09 16:48:30 +0900

    chore: 프로젝트 초기설정

---
+++ RxSwiftEx-1/RxSwiftEx-1UITests/RxSwiftEx_1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RxSwiftEx_1UITestsLaunchTests.swift
+//  RxSwiftEx-1UITests
+//
+//  Created by Bran on 2022/07/09.
+//
+
+import XCTest
+
+class RxSwiftEx_1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
