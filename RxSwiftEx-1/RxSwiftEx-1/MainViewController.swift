01693bfd834a42e9bf6229f88eee756c0152e627
Bran <cold929@naver.com>
2022-07-09 16:58:39 +0900

    chore: 프로젝트 초기설정 - 2

---
+++ RxSwiftEx-1/RxSwiftEx-1/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  RxSwiftEx-1
+//
+//  Created by Bran on 2022/07/09.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+class MainViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
