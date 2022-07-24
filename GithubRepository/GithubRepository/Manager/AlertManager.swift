a0da0df736cb9d579bea04037a91f50144ffff51
Bran <cold929@naver.com>
2022-07-25 01:03:55 +0900

    feat: 최상위 뷰에서 얼럿을 띄워주는 매니저 추가(에러 핸들링)

--- GithubRepository/GithubRepository/Manager/AlertManager.swift
+++ GithubRepository/GithubRepository/Manager/AlertManager.swift
@@ -5,4 +5,31 @@
 //  Created by Bran on 2022/07/25.
 //
 
-import Foundation
+import UIKit
+
+class AlertManager {
+  public static let shared = AlertManager()
+
+  private init() { }
+
+  func topViewController(
+    title: String,
+    message: String
+  ) {
+    DispatchQueue.main.async {
+      let keyWindow = UIApplication.shared.windows.filter { $0.isKeyWindow }.first
+      if var topController = keyWindow?.rootViewController {
+        while let presentedViewController = topController.presentedViewController { topController = presentedViewController }
+        let alert = UIAlertController(
+          title: title,
+          message: message,
+          preferredStyle: .alert
+        )
+
+        let action = UIAlertAction(title: "Close", style: .default)
+        alert.addAction(action)
+        topController.present(alert, animated: true)
+      }
+    }
+  }
+}
