021af3d2d0a376f9ba121e1ce76da9e2d44f125b
Sangwon Shin <cold929@naver.com>
2022-07-25 01:07:57 +0900

    Feature/GitHub (#7)
    * feat: input output 패턴 적용
    
    * fix: Network Manager 로직 수정
    
    * fix: input - output 패턴 수정(transform 과정에 subscribe 없도록)
    
    * feat: 최상위 뷰에서 얼럿을 띄워주는 매니저 추가(에러 핸들링)
    
    * chore: project Setting

--- GithubRepository/GithubRepository/Manager/NetworkManager.swift
+++ GithubRepository/GithubRepository/Manager/NetworkManager.swift
@@ -11,6 +11,8 @@ import RxSwift
 
 enum NetworkError: Error {
   case `default`
+  case invalidResponse(URLResponse?)
+  case invalidJSON
 }
 
 class NetworkManager {
@@ -18,20 +20,26 @@ class NetworkManager {
 
   private init() { }
 
-  func requestRepository(_ organization: String) -> Single<Result<[Github], Error>> {
+  func requestRepository(_ organization: String) -> Observable<[Github]> {
     let urlStr = "https://api.github.com/orgs/\(organization)/repos"
     return Observable.just(urlStr)
       .map { URL(string: $0)! }
       .map { URLRequest(url: $0) }
-      .flatMap { URLSession.shared.rx.data(request: $0) } // MARK: - Error가 발생하는 경우, thorw로 되어있는데 어디서 에러 핸들링을 해줘야 하는가?
-      .map { data -> Result<[Github], Error> in
+      .flatMap { URLSession.shared.rx.response(request: $0) }
+      .map { (result, data) -> Data in
+        if result.statusCode == 200 {
+          return data
+        } else {
+          throw NetworkError.invalidResponse(result)
+        }
+      }
+      .map { data -> [Github] in
         guard
           let githubData = try? JSONDecoder().decode([Github].self, from: data)
         else {
-          return .failure(NetworkError.default)
+          throw NetworkError.invalidJSON
         }
-        return .success(githubData)
+        return githubData
       }
-      .asSingle()
   }
 }
--- GithubRepository/GithubRepository/ViewModel/GithubViewModel.swift
+++ GithubRepository/GithubRepository/ViewModel/GithubViewModel.swift
@@ -11,11 +11,8 @@ import RxCocoa
 import RxSwift
 import RxRelay
 
-class GithubViewModel {
-
+final class GithubViewModel {
   private var disposeBag = DisposeBag()
-  let githubList = PublishSubject<[Github]>()
-  let errorList = PublishSubject<Error>()
 
   struct Input {
     let textFieldTapAction: Observable<String>
@@ -23,39 +20,27 @@ class GithubViewModel {
 
   struct Output {
     let organizationValue: Driver<[Github]>
-    let errorValue: Driver<Error>
   }
 
   func transform(input: Input) -> Output {
-    input.textFieldTapAction
-      .subscribe(onNext: { [weak self] find in
-        self?.fetchGithub(find)
-      })
-      .disposed(by: disposeBag)
-
+    let organizationValue = input.textFieldTapAction
+      .flatMap { [weak self] organization -> Observable<[Github]> in
+        guard let self = self else { return .empty() }
+        return self.fetchGithub(organization)
+      }
+      .asDriver(onErrorJustReturn: [])
     return Output(
-      organizationValue: githubList.asDriver(onErrorJustReturn: []),
-      errorValue: errorList.asDriver(onErrorJustReturn: NetworkError.default)
+      organizationValue: organizationValue
     )
   }
 }
 
 extension GithubViewModel {
-  func fetchGithub(_ organization: String) {
+  func fetchGithub(_ organization: String) -> Observable<[Github]> {
     NetworkManager.shared.requestRepository(organization)
-      .subscribe(
-        onSuccess: { [weak self] result in
-          switch result {
-          case .success(let data):
-            self?.githubList.onNext(data)
-          case .failure(let error):
-            print(error)
-          }
-        },
-        onFailure: { [weak self] error in
-          self?.errorList.onNext(error)
-        }
-      )
-      .disposed(by: disposeBag)
+      .catch { error in // catch 위치 여기인 이유 항상 생각하기
+        AlertManager.shared.topViewController(title: "Error", message: error.localizedDescription)
+        return .empty()
+      }
   }
 }
